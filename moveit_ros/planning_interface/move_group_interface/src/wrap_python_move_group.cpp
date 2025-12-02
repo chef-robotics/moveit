49303b10b956f109397a50ac02580f4339124d5f
cgeering <cgeering1203@gmail.com>
2025-12-02 14:13:25 -0800

    Reorg to make certain funcs private

--- moveit_ros/planning_interface/move_group_interface/src/wrap_python_move_group.cpp
+++ moveit_ros/planning_interface/move_group_interface/src/wrap_python_move_group.cpp
@@ -550,56 +550,6 @@ public:
     return py_bindings_tools::serializeMsg(constraints_msg);
   }
 
-  // Convert a Python list of deserialized wrench messages to std::vector<geometry_msgs::Wrench>
-  void convertListToArrayOfWrenches(const bp::list& wrenches, std::vector<geometry_msgs::Wrench>& result)
-  {
-    int len = bp::len(wrenches);
-    result.reserve(len);
-    for (int i = 0; i < len; ++i) {
-      const py_bindings_tools::ByteString& wrench_str = bp::extract<py_bindings_tools::ByteString>(wrenches[i]);
-      geometry_msgs::Wrench wrench_msg;
-      py_bindings_tools::deserializeMsg(wrench_str, wrench_msg);
-      result.push_back(wrench_msg);
-    }
-  }
-
-  /**
-   * \brief Determine the link names from a joint model group that match the link names in a KDL chain of the group.
-   *
-   * KDL chains have a "segment" for each (joint, child-link) pair starting from the root joint.
-   * There is no segment for the (root-joint, parent-link) pair, so the parent link is excluded
-   * from the returned list.
-   *
-   * \param group Joint model group to pull link names from.
-   *
-   * \return Vector of link names, excluding the root joint's parent link name.
-   */
-  std::vector<std::string> getKdlChainLinkNamesFromGroup(const robot_model::JointModelGroup* group)
-  {
-    if (!group)
-    {
-      return std::vector<std::string>();
-    }
-
-    // Determine the root joint's parent link name.
-    const auto root_joint = group->getCommonRoot();
-    const auto root_joint_parent_link = root_joint ? root_joint->getParentLinkModel() : nullptr;
-    const std::string root_joint_parent_link_name =
-        root_joint_parent_link ? root_joint_parent_link->getName() : std::string();
-
-    std::vector<std::string> link_names;
-    for (const auto& link_name : group->getLinkModelNames())
-    {
-      // Skip the root joint's parent link (if it exists) since it is not part of the KDL chain.
-      if (root_joint_parent_link && link_name == root_joint_parent_link_name)
-      {
-        continue;
-      }
-      link_names.push_back(link_name);
-    }
-    return link_names;
-  }
-
   /**
    * \brief Compute joint torques for each point in a RobotTrajectory message,
    *   and store in the point's `effort` field.
@@ -638,8 +588,7 @@ public:
     }
     if (external_link_wrenches.empty())
     {
-      const robot_model::JointModelGroup* group = robot_model->getJointModelGroup(group_name);
-      external_link_wrenches.resize(group ? group->getLinkModelNames().size() : 0);
+      external_link_wrenches.resize(getKdlChainLinkNamesFromGroup(robot_model->getJointModelGroup(group_name)).size());
     }
 
     // Release GIL and do the actual torque computation.
@@ -744,7 +693,7 @@ public:
     }
     if (external_link_wrenches.empty())
     {
-      external_link_wrenches.resize(getKdlChainLinkNamesFromGroup(ref_state_obj.getJointModelGroup(group_name)).size());
+      external_link_wrenches.resize(getKdlChainLinkNamesFromGroup(robot_model->getJointModelGroup(group_name)).size());
     }
 
     // Convert joint velocity, acceleration, and torque limits from Python to C++ objects.
@@ -902,6 +851,56 @@ private:
         point.effort = joint_torques;
       }
     }
+  }
+
+  // Convert a Python list of deserialized wrench messages to std::vector<geometry_msgs::Wrench>
+  void convertListToArrayOfWrenches(const bp::list& wrenches, std::vector<geometry_msgs::Wrench>& result)
+  {
+    int len = bp::len(wrenches);
+    result.reserve(len);
+    for (int i = 0; i < len; ++i) {
+      const py_bindings_tools::ByteString& wrench_str = bp::extract<py_bindings_tools::ByteString>(wrenches[i]);
+      geometry_msgs::Wrench wrench_msg;
+      py_bindings_tools::deserializeMsg(wrench_str, wrench_msg);
+      result.push_back(wrench_msg);
+    }
+  }
+
+  /**
+   * \brief Determine the link names from a joint model group that match the link names in a KDL chain of the group.
+   *
+   * KDL chains have a "segment" for each (joint, child-link) pair starting from the root joint.
+   * There is no segment for the (root-joint, parent-link) pair, so the parent link is excluded
+   * from the returned list.
+   *
+   * \param group Joint model group to pull link names from.
+   *
+   * \return Vector of link names, excluding the root joint's parent link name.
+   */
+  std::vector<std::string> getKdlChainLinkNamesFromGroup(const robot_model::JointModelGroup* group)
+  {
+    if (!group)
+    {
+      return std::vector<std::string>();
+    }
+
+    // Determine the root joint's parent link name.
+    const auto root_joint = group->getCommonRoot();
+    const auto root_joint_parent_link = root_joint ? root_joint->getParentLinkModel() : nullptr;
+    const std::string root_joint_parent_link_name =
+        root_joint_parent_link ? root_joint_parent_link->getName() : std::string();
+
+    std::vector<std::string> link_names;
+    for (const auto& link_name : group->getLinkModelNames())
+    {
+      // Skip the root joint's parent link (if it exists) since it is not part of the KDL chain.
+      if (root_joint_parent_link && link_name == root_joint_parent_link_name)
+      {
+        continue;
+      }
+      link_names.push_back(link_name);
+    }
+    return link_names;
   }
 };
 
